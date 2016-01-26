56960acf35a9dff68ab88105fd5ef048f04f7c86
Louis Wilbrink <louis.wilbrink@gmail.com>
2016-01-26 08:04:22 -0700

    separate controllers into individual files; switch bower_components location

---
+++ www/js/controllers/contacts-list.js
@@ -0,0 +1,77 @@
+angular.module('intro.controllers.contactsList', [])
+.controller('ContactsListCtrl', function($scope, $cordovaContacts, $ionicPlatform, Contacts, $cordovaSms) {
+
+  $scope.contactsList = [
+    { name: 'Bob', phoneNumber: 15551234 },
+    { name: 'Gretchen', phoneNumber: 15551234 },
+    { name: 'Sally', phoneNumber: 15551234 },
+    { name: 'Amos', phoneNumber: 15551234 }
+  ];
+
+  $scope.sendMessage = function (phoneNumber) {
+    console.log('sending a message to ', phoneNumber);
+
+    $cordovaSms
+      .send('9737681848', 'Hey this is working!', {})
+      .then(function() {
+        console.log('sent!');
+      }, function(error) {
+        console.log(error);
+      });
+  };
+
+  function searchByNumberType (phoneNumbers, type) {
+
+    var mobileNumber;
+
+    angular.forEach(phoneNumbers, function (phoneNumber) {
+      if (phoneNumber.type === type) {
+        mobileNumber = phoneNumber.value;
+      }
+    });
+
+    return mobileNumber;
+  }
+
+  function getMobileNumber (phoneNumbers) {
+
+    var mobileNumber;
+
+    mobileNumber = searchByNumberType(phoneNumbers, 'mobile');
+
+    if (!mobileNumber) {
+      mobileNumber = searchByNumberType(phoneNumbers, 'home');
+    }
+
+    if (!mobileNumber) {
+      mobileNumber = searchByNumberType(phoneNumbers, 'work');
+    }
+
+    return mobileNumber;
+  }
+
+  function onSuccess(contacts) {
+    angular.forEach(contacts, function (contact, index) {
+      $scope.contactsList.push({
+        name: contact.name.formatted, 
+        phoneNumber: getMobileNumber(contact.phoneNumbers)
+      });
+    });
+  }
+
+  function onError (error) {
+    alert(error);
+  }
+
+  var options = {};
+  options.multiple = true;
+
+  $ionicPlatform.ready(function(){
+    console.log('ionicPlatform ready!');
+  });
+
+  $scope.getContacts = function () {
+    $cordovaContacts.find(options).then(onSuccess, onError);
+  }
+});
+
---
+++ www/js/controllers/playlist.js
@@ -0,0 +1,5 @@
+angular.module('intro.controllers.playlist', [])
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+  console.log('In PlaylistCtrl');
+});
