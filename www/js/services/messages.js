e6c8ec4eafc8fe027c25443720f8aa9840d6023a
Louis Wilbrink <louis.wilbrink@gmail.com>
2016-02-18 21:48:40 -0700

    styled /confirmation page

--- www/js/services/messages.js
+++ www/js/services/messages.js
@@ -9,12 +9,12 @@ angular.module('services.messages', [])
   function messagesService () {
 
     var messages = [{
-      text: 'Hey ${p0} and ${p1}, wanted to introduce you guys since you\'re both in the ${i0} industry',
+      text: 'Hey ${p0} and ${p1}, wanted to introduce you guys since you\'re both in the real estate industry',
       blanks: {
         p: 2
       }
     }, {
-      text: 'Hi ${p0}, wanted to introduce you to ${p1}.  ${p1} is a good friend of mine from ${l0}',
+      text: 'Hi ${p0}, wanted to introduce you to ${p1}.  ${p1} is a good friend of mine from college',
       blanks: {
         p: 2
       }
