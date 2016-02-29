083bc3217bcca6e891ff5549299eab5da4e34de2
Louis Wilbrink <louis.wilbrink@gmail.com>
2016-02-28 17:52:53 -0700

    refactor db.createUser to use all promises; make login method public, allow caller to handle promise result; test login using test-button

--- www/js/controllers/test-button.js
+++ www/js/controllers/test-button.js
@@ -1,7 +1,19 @@
 angular.module('controllers.testButton', [])
 .controller('TestButtonCtrl', function($scope, Db) {
-  console.log(Db);
 
   $scope.createUser = Db.createUser;
+
+  $scope.login = function (email, password) {
+    Db.login({
+      email: email,
+      password: password
+    })
+    .then(function (something) {
+      console.log('Logged in', something);
+    })
+    .catch(function (error) {
+      console.log('Error Logging In', error);
+    });
+  };
 });
 
--- www/js/services/db.js
+++ www/js/services/db.js
@@ -8,63 +8,92 @@ angular.module('services.db', ['firebase'])
 
   function DbService ($firebaseAuth, $firebaseObject, $q, $timeout) {
 
+    /* PRIVATE VARIABLES */
+
     // Create firebase reference to endpoint, then create authentication
     // reference.
     var ref = new Firebase('https://introgo-prod.firebaseio.com/');
     var auth = $firebaseAuth(ref);
 
+    /* PRIVATE METHODS */
+
+    function addUser (userInfo) {
+
+      var isUserAdded = $q.defer();
+
+      // Create user reference using uid.
+      var user = new Firebase('https://introgo-prod.firebaseio.com/users/' + userInfo.uid);
+
+      // Add user to db.
+      user.set({
+        firstName: 'Louis',
+        lastName: 'Wilbrink',
+        phone: '1234567890',
+        uuid: 'asdf1234poiu0987',
+        email: userInfo.email,
+        connections: [{
+          introductees: [{ dummy: 'dummy' }],
+          message: 'dummy message'
+        }],
+        customMessages: [
+          'Hello ${p1} and ${p2}, you guy\'s should meet',
+          '${p1}, meet ${p2}'
+        ]
+      }, function (error) {
+        if (!error) {
+          isUserAdded.resolve(true);
+        }
+        else {
+          isUserAdded.reject(error);
+        }
+      });
+
+      return isUserAdded.promise;
+    }
+
+    function getLoginInfo (authData) {
+      return $q.when({
+        uid: authData.uid,
+        email: authData.password.email
+      });
+    }
+
+    /* PUBLIC METHODS */
+
+    function login (credentials) {
+      return auth.$authWithPassword({
+        email: credentials.email,
+        password: credentials.password
+      })
+    }
+
+    function createUser (email, password) {
+      var credentials = {
+        email: email,
+        password: password
+      };
+
+      auth.$createUser(credentials)
+      .then(function passCredentials (userData) {
+        return $q.when(credentials);
+      })
+      .then(login)        // receives credentials.
+      .then(getLoginInfo) // receives authData.
+      .then(addUser)      // receives userInfo.
+      .catch(function (error) {
+        console.log('Error Creating User:', error);
+      });
+    }
+
     return {
-      createUser: function () {
-
-        auth.$createUser({
-          email: 'lw@lw.com',
-          password: 'lw'
-        }).then(function(userData) {
-          console.log("User " + userData.uid + " created successfully!");
-
-          // Log in.
-
-          auth.$authWithPassword({
-            email: 'lw@lw.com',
-            password: 'lw'
-          }).then(function (authData) {
-            console.log('logged in (authData)', authData);
-            
-            // Create user reference using uid.
-            var user = new Firebase('https://introgo-prod.firebaseio.com/users/' + userData.uid);
-
-            // Add user to db.
-            user.set({
-              firstName: 'Louis',
-              lastName: 'Wilbrink',
-              phone: '1234567890',
-              uuid: 'asdf1234poiu0987',
-              email: 'lw@lw.com',
-              connections: [{
-                introductees: [{ dummy: 'dummy' }],
-                message: 'dummy message'
-              }],
-              customMessages: [
-                'Hello ${p1} and ${p2}, you guy\'s should meet',
-                '${p1}, meet ${p2}'
-              ]
-            }, function (error) {
-              if (!error) {
-                console.log('Success');
-              }
-              else {
-                console.log('Error adding new user');
-              }
-            });
-          }).catch(function (error) {
-            console.log('Error logging in', error);
-          });
-
-        })
-        .catch(function (error) {
-          console.log('error creating user', error);
-        });
-      },
+      createUser: createUser,
+      login: login,
+      removeUser: null,
+      addIntroduction: null,
+      updateFirstName: null,
+      updateLastName: null,
+      updatePhone: null,
+      updateEmail: null,
       logout: function () {
         return ref.unauth();
       }
